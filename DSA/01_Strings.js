1921257692dcd2f463c2ce8d0455e202f5c76d1d
srzorro <srzorro@gmail.com>
2024-02-15 15:46:05 -0500

    :clown_face: Ejercicio 04_Arrays.DSA

--- DSA/01_Strings.js
+++ DSA/01_Strings.js
@@ -5,8 +5,8 @@ An integer is a palindrome when it reads the same forward and backward
 
 */
 
-const isPalindrome = function(word){
-  return x < 0 ? false ? x === +x.toString().split("").reverse().join("");   
-};
+const isPalindrome = function(x) {
+  return x >= 0 && x.toString() === x.toString().split("").reverse().join("");
+}
 const res = isPalindrome(232);
-console.log(res); // true 
+console.log(res); 
--- DSA/04_Arrays.js
+++ DSA/04_Arrays.js
@@ -1,5 +1,23 @@
 // Maximum Sum Subarray
+/*
+Dada una matriz de numeros, encuentre la submatriz con la suma mas grande y devuelve la suma
+*/
 
+function maxSubArray(nums) {
+  let maxSum = nums[0];
+  let currentSum = nums[0];
 
+  for (let i = 1; i < nums.length; i++) {
+      currentSum = Math.max(nums[i], currentSum + nums[i]);
+      maxSum = Math.max(maxSum, currentSum);
+  }
+
+  return maxSum;
+}
+
+// Test cases
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])); // Output: 6
+console.log(maxSubArray([1])); // Output: 1
+console.log(maxSubArray([5,4,-1,7,8])); // Output: 23
 
 
